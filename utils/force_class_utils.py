1cc952d53c77488a961aaf99370ad5951dc33e74
Sebastian Valencia <sebastian.valencia@lup-umwelt.de>
2025-11-06 15:11:21 +0000

    prm file creation, requirementes and readme

--- utils/force_class_utils.py
+++ utils/force_class_utils.py
@@ -113,8 +113,13 @@ def force_class(project_name, force_dir, local_dir, base_path, aois, hold):
 
         shutil.copy(f"{base_path_script}/utils/skel/force_cube_sceleton/datacube-definition.prj",f"{base_path}/process/temp/{project_name}/FORCE/{basename}/datacube-definition.prj")
         shutil.copy(f"{base_path_script}/utils/skel/force_cube_sceleton/datacube-definition.prj",f"{base_path}/process/temp/{project_name}/FORCE/{basename}/tiles_tss/datacube-definition.prj")
-        shutil.copy(f"{base_path_script}/utils/skel/TSA_NoCom.prm", f"{base_path}/process/temp/{project_name}/FORCE/{basename}/tsa.prm")
 
+        tsa_param = f"{base_path}/process/temp/{project_name}/FORCE/{basename}/tsa.prm"
+        param_cmd = (
+            f'sudo docker run -v {local_dir} -v {force_dir} -u "$(id -u):$(id -g)" '
+            f'davidfrantz/force:latest force-parameter {tsa_param} TSA'
+        )
+        subprocess.run(['bash', '-lc', param_cmd], check=True)
 
         X_TILE_RANGE, Y_TILE_RANGE = extract_coordinates(f"{base_path}/process/temp/{project_name}/FORCE/{basename}/tile_extent.txt")
         # Define replacements
@@ -129,6 +134,12 @@ def force_class(project_name, force_dir, local_dir, base_path, aois, hold):
             # PROCESSING EXTENT AND RESOLUTION
             f'X_TILE_RANGE = 0 0': f'X_TILE_RANGE = {X_TILE_RANGE}',
             f'Y_TILE_RANGE = 0 0': f'Y_TILE_RANGE = {Y_TILE_RANGE}',
+            f"STREAMING = TRUE" : f"STREAMING = FALSE",
+            f"OUTPUT_SUBFOLDERS = FALSE": f"OUTPUT_SUBFOLDERS = TRUE",
+            f"CHUNK_SIZE = 0 0": f"CHUNK_SIZE = 100 100",
+            f"ABOVE_NOISE = 0": f"ABOVE_NOISE = 3",
+            f"BELOW_NOISE = 0": f"BELOW_NOISE = 1",
+            f"OUTPUT_TSS = FALSE": f"OUTPUT_TSS = TRUE",
         }
         # Replace parameters in the file
         replace_parameters(f"{base_path}/process/temp/{project_name}/FORCE/{basename}/tsa.prm", replacements)
@@ -214,10 +225,15 @@ def force_class_udf(project_name, force_dir, local_dir, base_path, aois, hold):
                     f"{base_path}/process/temp/{project_name}/FORCE/{basename}/datacube-definition.prj")
         shutil.copy(f"{base_path_script}/utils/skel/force_cube_sceleton/datacube-definition.prj",
                     f"{base_path}/process/temp/{project_name}/FORCE/{basename}/tiles_tss/datacube-definition.prj")
-        shutil.copy(f"{base_path_script}/utils/skel/UDF_NoCom.prm",
-                    f"{base_path}/process/temp/{project_name}/FORCE/{basename}/tsa_UDF.prm")
         shutil.copy(f"{base_path_script}/utils/skel/udf_pixel.py",
                     f"{base_path}/process/temp/{project_name}/FORCE/{basename}/UDF_pixel.py")
+
+        tsa_param = f"{base_path}/process/temp/{project_name}/FORCE/{basename}/tsa_UDF.prm"
+        param_cmd = (
+            f'sudo docker run -v {local_dir} -v {force_dir} -u "$(id -u):$(id -g)" '
+            f'davidfrantz/force:latest force-parameter {tsa_param} UDF'
+        )
+        subprocess.run(['bash', '-lc', param_cmd], check=True)
 
         X_TILE_RANGE, Y_TILE_RANGE = extract_coordinates(f"{base_path}/process/temp/{project_name}/FORCE/{basename}/tile_extent.txt")
 
@@ -233,7 +249,13 @@ def force_class_udf(project_name, force_dir, local_dir, base_path, aois, hold):
             # PROCESSING EXTENT AND RESOLUTION
             f'X_TILE_RANGE = 0 0': f'X_TILE_RANGE = {X_TILE_RANGE}',
             f'Y_TILE_RANGE = 0 0': f'Y_TILE_RANGE = {Y_TILE_RANGE}',
+            f"STREAMING = TRUE": f"STREAMING = FALSE",
+            f"OUTPUT_SUBFOLDERS = FALSE": f"OUTPUT_SUBFOLDERS = TRUE",
+            f"CHUNK_SIZE = 0 0": f"CHUNK_SIZE = 100 100",
+            f"ABOVE_NOISE = 0": f"ABOVE_NOISE = 3",
+            f"BELOW_NOISE = 0": f"BELOW_NOISE = 1",
             f"FILE_PYTHON = NULL": f"FILE_PYTHON = {base_path}/process/temp/{project_name}/FORCE/{basename}/UDF_pixel.py",
+            f"OUTPUT_PYP = FALSE": f"OUTPUT_PYP = TRUE",
         }
         # Replace parameters in the file
         replace_parameters(f"{base_path}/process/temp/{project_name}/FORCE/{basename}/tsa_UDF.prm", replacements)
